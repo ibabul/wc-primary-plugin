e3b447eba66e30222882419a07f4ce00e04246ac
ibabul <wpimtiaz@gmail.com>
2020-07-16 04:34:09 +0600

    initial commit

--- WooCommerce-startup-plugin.php
+++ WooCommerce-startup-plugin.php
@@ -12,17 +12,16 @@
 
  //check to make sure WooCommerce is active
 
+ require_once __DIR__ . '/vendor/autoload.php';
+
+ 
+
  if ( in_array('woocommerce/woocommerce.php' , apply_filters('active_plugins' , get_option('active_plugins') ) ) ) {
    // Only run if there's no other class with this name
 
-   if (! class_exists('WC_Example') ) {
-      class WC_Example{
-         public function __construct()
-         {
-            echo 'Hello World';
-         }
+   if (! class_exists('Basic') ) {
 
-      }
-      $GLOBALS['wc_example'] = new WC_Example();
-   }
+    new WooCommerce\Startup\Basic();
+   
  }
+}
---
+++ includes/Basic.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace WooCommerce\Startup;
+
+Class Basic {
+   public function __construct()
+         {
+            echo 'Hello World';
+         }
+}
